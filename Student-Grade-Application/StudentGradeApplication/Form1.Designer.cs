4d1f1571168e56badea9c73b7be283ed8f8fa40b
s3rb0g <raniegobres27@gmail.com>
2024-07-09 09:37:42 +0800

    Add files via upload

---
+++ Student-Grade-Application/StudentGradeApplication/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace StudentGradeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textEnglish = new System.Windows.Forms.TextBox();
+            this.textMath = new System.Windows.Forms.TextBox();
+            this.textScience = new System.Windows.Forms.TextBox();
+            this.textFilipino = new System.Windows.Forms.TextBox();
+            this.textHistory = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.messageshow = new System.Windows.Forms.Label();
+            this.messageshow2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "English";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Math";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Science";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Filipino";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "History";
+            // 
+            // textName
+            // 
+            this.textName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textName.Location = new System.Drawing.Point(134, 13);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(252, 30);
+            this.textName.TabIndex = 6;
+            // 
+            // textEnglish
+            // 
+            this.textEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEnglish.Location = new System.Drawing.Point(134, 96);
+            this.textEnglish.Name = "textEnglish";
+            this.textEnglish.Size = new System.Drawing.Size(117, 30);
+            this.textEnglish.TabIndex = 7;
+            // 
+            // textMath
+            // 
+            this.textMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMath.Location = new System.Drawing.Point(134, 145);
+            this.textMath.Name = "textMath";
+            this.textMath.Size = new System.Drawing.Size(117, 30);
+            this.textMath.TabIndex = 8;
+            // 
+            // textScience
+            // 
+            this.textScience.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textScience.Location = new System.Drawing.Point(134, 194);
+            this.textScience.Name = "textScience";
+            this.textScience.Size = new System.Drawing.Size(117, 30);
+            this.textScience.TabIndex = 9;
+            // 
+            // textFilipino
+            // 
+            this.textFilipino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFilipino.Location = new System.Drawing.Point(134, 245);
+            this.textFilipino.Name = "textFilipino";
+            this.textFilipino.Size = new System.Drawing.Size(117, 30);
+            this.textFilipino.TabIndex = 10;
+            // 
+            // textHistory
+            // 
+            this.textHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHistory.Location = new System.Drawing.Point(134, 293);
+            this.textHistory.Name = "textHistory";
+            this.textHistory.Size = new System.Drawing.Size(117, 30);
+            this.textHistory.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(152, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Grades";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(18, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 60);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Generate Average";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // messageshow
+            // 
+            this.messageshow.AutoSize = true;
+            this.messageshow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageshow.Location = new System.Drawing.Point(443, 165);
+            this.messageshow.Name = "messageshow";
+            this.messageshow.Size = new System.Drawing.Size(62, 25);
+            this.messageshow.TabIndex = 14;
+            this.messageshow.Text = "          ";
+            // 
+            // messageshow2
+            // 
+            this.messageshow2.AutoSize = true;
+            this.messageshow2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageshow2.Location = new System.Drawing.Point(358, 209);
+            this.messageshow2.Name = "messageshow2";
+            this.messageshow2.Size = new System.Drawing.Size(62, 25);
+            this.messageshow2.TabIndex = 15;
+            this.messageshow2.Text = "          ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageshow2);
+            this.Controls.Add(this.messageshow);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textHistory);
+            this.Controls.Add(this.textFilipino);
+            this.Controls.Add(this.textScience);
+            this.Controls.Add(this.textMath);
+            this.Controls.Add(this.textEnglish);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student Grade Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textEnglish;
+        private System.Windows.Forms.TextBox textMath;
+        private System.Windows.Forms.TextBox textScience;
+        private System.Windows.Forms.TextBox textFilipino;
+        private System.Windows.Forms.TextBox textHistory;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label messageshow;
+        private System.Windows.Forms.Label messageshow2;
+    }
+}
+
---
+++ Student-Grade-Application/StudentGradeApplication/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double english = double.Parse(textEnglish.Text);
+            double math = double.Parse(textMath.Text);
+            double science = double.Parse(textScience.Text);
+            double filipino = double.Parse(textFilipino.Text);
+            double history = double.Parse(textHistory.Text);
+
+            double average = (english + math + science + filipino + history) / 5;
+            average = Math.Round(average,2);
+
+            string message = average >= 75.00 ? "passed." : "failed.";
+
+            messageshow.Text = "The student " + message;
+            string name = textName.Text;
+            messageshow2.Text = "The averange of " + name + " is " + average;
+
+
+
+
+
+
+        }
+    }
+}
